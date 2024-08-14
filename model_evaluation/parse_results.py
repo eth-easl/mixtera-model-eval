3dbcf649ab5d6f4c5cc92c6c8b9aa9f0705f572e
Maximilian Böther <maxi+git@boether.de>
2024-08-14 14:02:27 +0200

    format and further run benchmark impl

--- model_evaluation/parse_results.py
+++ model_evaluation/parse_results.py
@@ -39,7 +39,6 @@ def collect_and_export_results(output_dir: Path):
         nshot = int(nshot_dir.name)
         result_dirs = [d for d in nshot_dir.glob("results_*") if d.is_dir()]
 
-        
         schema_set = set()  # To track consistency of schema across all JSON files
 
         for dir in result_dirs:
@@ -57,10 +56,10 @@ def collect_and_export_results(output_dir: Path):
 
     if all_results:
         df = pd.DataFrame(all_results)
-        df[['checkpoint_id', 'nshot']] = df[['checkpoint_id', 'nshot']].apply(pd.to_numeric)
-        column_order = ['checkpoint_id', 'nshot'] + [col for col in df.columns if col not in ['checkpoint_id', 'nshot']]
+        df[["checkpoint_id", "nshot"]] = df[["checkpoint_id", "nshot"]].apply(pd.to_numeric)
+        column_order = ["checkpoint_id", "nshot"] + [col for col in df.columns if col not in ["checkpoint_id", "nshot"]]
         df = df[column_order]
-        df.sort_values(by=['checkpoint_id', 'nshot'], inplace=True)
+        df.sort_values(by=["checkpoint_id", "nshot"], inplace=True)
         df.to_csv(output_dir / "results.csv", index=False)
         typer.echo(f"Results have been successfully written to '{output_dir / 'results.csv'}'.")
     else:
--- performance_benchmarks/run_benchmark.py
+++ performance_benchmarks/run_benchmark.py
@@ -1,9 +1,7 @@
 from enum import Enum
-from typing import Literal
 import typer
 from pathlib import Path
 import yaml
-from tqdm import tqdm
 import subprocess
 import os
 import itertools
@@ -20,38 +18,41 @@ app = typer.Typer()
 
 # Machine: RTX 3090, H100, GH200
 # Model: TinyLlama 1B, Llama7B, Llamba70B
-    # Model+Machine fixes pp and tp => defines base config
-    # TODO: Find out parameters for LLama7B and 70B. 
-    # Llama2 vs 3? do they define different architecture params?
-    # what learning rate schedule to use? (probably irrelevant for tput though)
+# Model+Machine fixes pp and tp => defines base config
+# TODO: Find out parameters for LLama7B and 70B.
+# Llama2 vs 3? do they define different architecture params?
+# what learning rate schedule to use? (probably irrelevant for tput though)
 # Global batch size: Accumulation step, micro batch size
-    # Might depend on machine/model combination?
+# Might depend on machine/model combination?
 # Sequence Length: 1024, 2048, 4096
-    # Does sequence length influence batch size?
-    # (it should, for the same batch size if we double num tokens per sec we double the amount of data fed into the model
+# Does sequence length influence batch size?
+# (it should, for the same batch size if we double num tokens per sec we double the amount of data fed into the model
 # Datalocation: Locally on NVMe (if applicable, probably only our machine), NFS
 # Interface: Huggingface mapped, Huggingface Iterable, Nanoset, Mosaic Streaming, WebDatasets, Mixtera
-# Data-Loading Workers: 0,1,4,16,32 
+# Data-Loading Workers: 0,1,4,16,32
 # dp instances: 1,2,4,8,16
-    # for sgs-gpu machines, there is an inherent constraint since we only try single node
-    # for swiss ai, let's see how much we can scale up with the baselines and then with mixtera
+# for sgs-gpu machines, there is an inherent constraint since we only try single node
+# for swiss ai, let's see how much we can scale up with the baselines and then with mixtera
 # Mixtera-Specific Parameters: Mixture Window, Chunk Workers, stream via server, local vs server (only for dp=1, tinyllama 1b)
 
 # This script should build up the cross product of all configurations
 # Then execute each configuration
-    # Either: sgs-gpu machine OR SwissAI cluster
+# Either: sgs-gpu machine OR SwissAI cluster
 # Each training should run for 5-10 minutes (configurable?)
 # Then, collect results (either wandb and/or local logging added to nanotron) after each run, build up csv with results, always persist.
+
 
 class MachineType(str, Enum):
     sgsrtx = "sgsrtx"
     sgsh100 = "sgsh100"
     cscs = "cscs"
 
+
 class ModelType(str, Enum):
     tinyllama = "tinyllama"
     llama7b = "llama7b"
     llama70b = "llama70b"
+
 
 # TODO: Find maximum microbatch size per GPU/model combination
 MACHINE_MODEL_MICROBATCH = {
@@ -73,12 +74,12 @@ MACHINE_MODEL_MICROBATCH = {
 }
 
 
-
 def check_nanotron_availability():
     try:
         global nanotron
         global run_script_path
         import nanotron
+
         run_script_path = Path(nanotron.__path__[0]).parent.parent / "run_train.py"
     except ImportError:
         typer.echo(
@@ -86,12 +87,13 @@ def check_nanotron_availability():
         )
         raise typer.Exit(code=1)
 
+
 def ask_to_continue():
     response = input("Do you want to continue? (yes/no) [yes]: ").strip().lower()
-    
-    if response == '' or response.startswith('y'):
+
+    if response == "" or response.startswith("y"):
         return True
-    elif response.startswith('n'):
+    elif response.startswith("n"):
         return False
     else:
         print("Invalid input, assuming 'yes'.")
@@ -110,24 +112,23 @@ def load_yaml_from_file(path: str | Path):
             typer.echo("Error: " + str(exc))
             raise typer.Exit(code=1) from exc
 
+
 def get_data_from_wandb(project: str, run_id: str) -> dict:
     api = wandb.Api()
     runs = api.runs(project)
-    run = next((run for run in runs if run.name.split('_', 2)[-1].startswith(run_id)), None)
+    run = next((run for run in runs if run.name.split("_", 2)[-1].startswith(run_id)), None)
 
     if not run:
         typer.echo(f"Error: Could not find run {run_id} in runs {[run.name for run in runs]}.")
         raise typer.Exit(code=1)
-    
+
     return run.history().to_json()
 
 
-
 def run_benchmark_on_sgs(config: dict, mode: MachineType) -> dict:
-
     # Persist yaml to disk
     bm_config_path = SCRIPT_DIR / "benchmark.yaml"
-    with open(bm_config_path, 'w+') as ff:
+    with open(bm_config_path, "w+") as ff:
         yaml.dump_all(config, ff)
 
     # Run nanotron training on the current node
@@ -139,33 +140,41 @@ def run_benchmark_on_sgs(config: dict, mode: MachineType) -> dict:
     nprocs = str(dp * tp * pp)
 
     command = [
-        'python', '-u', '-m', 'torch.distributed.run',
-        f'--nproc_per_node={nprocs}', str(run_script_path), '--config-file', str(bm_config_path)
+        "python",
+        "-u",
+        "-m",
+        "torch.distributed.run",
+        f"--nproc_per_node={nprocs}",
+        str(run_script_path),
+        "--config-file",
+        str(bm_config_path),
     ]
     typer.echo(f"Running benchmark with command {command}")
     subprocess.run(command, check=True)
-    typer.echo(f"Benchmark done, collecting data")
+    typer.echo("Benchmark done, collecting data")
     results = get_data_from_wandb(config["general"]["project"], config["general"]["run"])
-    typer.echo(f"Data collected")
+    typer.echo("Data collected")
     bm_config_path.unlink()
 
     return results
 
+
 def run_benchmark(config: dict, mode: MachineType) -> dict:
     if mode in [MachineType.sgsh100, MachineType.sgsrtx]:
         return run_benchmark_on_sgs(config, mode)
-    
+
     typer.echo(f"Error: No implementation yet for mode `{mode}`")
     raise typer.Exit(code=1)
 
 
-def validate_user_input(mode: MachineType,
+def validate_user_input(
+    mode: MachineType,
     model: ModelType,
 ):
     if mode == MachineType.sgsrtx and model not in [ModelType.tinyllama, ModelType.llama7b]:
         typer.echo(f"RTX 3090 machines don't support model {model}")
         return False
-    
+
     if mode in [MachineType.sgsrtx, MachineType.sgsh100]:
         check_nanotron_availability()
 
@@ -179,18 +188,20 @@ def validate_user_input(mode: MachineType,
             typer.echo("Note that you selected H100 gpus but this is not sgs-gpu07.")
             if not ask_to_continue():
                 raise typer.Exit(code=1)
-        
-        if mode == MachineType.sgsrtx and hostname not in ["sgs-gpu01","sgs-gpu02","sgs-gpu03","sgs-gpu04" ]:
+
+        if mode == MachineType.sgsrtx and hostname not in ["sgs-gpu01", "sgs-gpu02", "sgs-gpu03", "sgs-gpu04"]:
             typer.echo("Note that you selected RTX3090 gpus but this is not sgs-gpu[01-04].")
             if not ask_to_continue():
                 raise typer.Exit(code=1)
 
+
 def load_base_config(model: ModelType) -> dict:
     if model == ModelType.tinyllama:
         return load_yaml_from_file(SCRIPT_DIR / "tinyllama.yaml")
-    
+
     typer.echo(f"Error: No config yet for model `{model}`")
     raise typer.Exit(code=1)
+
 
 def adjust_base_config(
     base_config: dict,
@@ -202,7 +213,7 @@ def adjust_base_config(
     dl_worker: int,
     dp: int,
     seq_length: int,
-    seed: int
+    seed: int,
 ) -> tuple[dict, dict]:
     config = deepcopy(base_config)
 
@@ -214,15 +225,15 @@ def adjust_base_config(
     config["parallelism"]["dp"] = dp
 
     # set microbatch size
-    # TODO: We need to figure out whether this matters. Can we always leave that at 1 for throughput benchmarks? For training, we don't want too small batches becaues that hurts convergence. However, for tput, in my understanding, increasing it to 2 will double the factual batch size, but keep the number of forward passes. So it should not affect throughput. Need to benchmark that though.
-    config["tokens"]["batch_accumulation_per_replica"] = 1 
+    # TODO: We need to figure out whether batch_accumulation_per_replica matters. Can we always leave that at 1 for throughput benchmarks? For training, we don't want too small batches becaues that hurts convergence. However, for tput, in my understanding, increasing it to 2 will double the factual batch size, but keep the number of forward passes. So it should not affect throughput. Need to benchmark that though.
+    config["tokens"]["batch_accumulation_per_replica"] = 1
     config["tokens"]["micro_batch_size"] = MACHINE_MODEL_MICROBATCH[mode][model]
 
     # set sequence length
     config["tokens"]["sequence_length"] = seq_length
 
     # set number of data loading workers
-    assert len(config["data_stages"]) == 1, f"data stages should only be 1"
+    assert len(config["data_stages"]) == 1, "data stages should only be 1"
     config["data_stages"][0]["data"]["num_loading_workers"] = dl_worker
     # set seed
     config["data_stages"][0]["data"]["seed"] = seed
@@ -232,13 +243,19 @@ def adjust_base_config(
 
     # number of tokens that we want to consume (will be rounded up to match batch size/seq length)
     # note after running some benchmarks we probably want to scale this based on the GPU type
-    total_tokens = 2,000,000 * dp
+    total_tokens = 2, 000, 000 * dp
     batch_size = dp * config["tokens"]["batch_accumulation_per_replica"] * config["tokens"]["micro_batch_size"]
     tokens_per_step = batch_size * seq_length
     train_steps = math.ceil(total_tokens / tokens_per_step)
     config["tokens"]["train_steps"] = train_steps
 
-    additional_info = {"total_tokens": total_tokens, "tokens_per_step": tokens_per_step, "batch_size": batch_size, "skipped": False, "skip_reason": ""}
+    additional_info = {
+        "total_tokens": total_tokens,
+        "tokens_per_step": tokens_per_step,
+        "batch_size": batch_size,
+        "skipped": False,
+        "skip_reason": "",
+    }
 
     # now decide whether this run should be skipped
     total_nodes = dp * int(config["parallelism"]["tp"]) * int(config["parallelism"]["pp"])
@@ -248,11 +265,13 @@ def adjust_base_config(
 
     return config, additional_info
 
+
 def convert_subdicts_to_json(d):
     for key, value in d.items():
         if isinstance(value, dict):
             d[key] = json.dumps(value)
     return d
+
 
 def persist_results_to_csv(output: Path, all_results: list[dict]):
     copied = deepcopy(all_results)
@@ -260,29 +279,32 @@ def persist_results_to_csv(output: Path, all_results: list[dict]):
     df = pd.DataFrame(data)
     df.to_csv(output, index=False)
 
+
 @app.command()
 def run_benchmarks(
     output_dir: Path,
-    run_id: str,
+    benchmark_name: str,
     mode: MachineType,
     model: ModelType,
     dataset_path: Path,
-    dl_workers: list[int] = [0,1,4,16,32],
-    dps: list[int] = [1,2,4,8,16],
+    dl_workers: list[int] = [0, 1, 4, 16, 32],
+    dps: list[int] = [1, 2, 4, 8, 16],
     seq_lengths: list[int] = [1024, 2048, 4096],
-    seeds: list[int] = [42]
+    seeds: list[int] = [42],
 ):
     if not validate_user_input(mode, model):
         raise typer.Exit(code=1)
-    
-    output_file = output_dir / f"{run_id}.csv"
+
+    output_file = output_dir / f"{benchmark_name}.csv"
     base_config = load_base_config(model)
     all_results = []
     bm_identifier = f"mixterabench_{current_milli_time()}"
     curr_run = 0
 
-    for (seed, dl_worker, dp, seq_len) in itertools.product(seeds, dl_workers, dps, seq_lengths):
-        adjusted_config, additional_info = adjust_base_config(base_config, dataset_path, bm_identifier, curr_run, mode, model, dl_worker, dp, seq_len, seed)
+    for seed, dl_worker, dp, seq_len in itertools.product(seeds, dl_workers, dps, seq_lengths):
+        adjusted_config, additional_info = adjust_base_config(
+            base_config, dataset_path, bm_identifier, curr_run, mode, model, dl_worker, dp, seq_len, seed
+        )
         base_results = {"config": adjusted_config} | additional_info
 
         if additional_info["skipped"]:
@@ -293,9 +315,6 @@ def run_benchmarks(
 
         persist_results_to_csv(output_file, all_results)
         curr_run += 1
-
-
-
 
 
 if __name__ == "__main__":
